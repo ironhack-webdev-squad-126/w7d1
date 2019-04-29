4db09e4cf93354e2a42e09d9ff31e8527de8f307
mjarraya <mjarraya@student.42.fr>
2019-04-29 12:09:59 +0200

    react hello world

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// src/index.js
+
+import React from 'react';
+// const React = require('react')
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>HELLO WORLD!</h1>,
+    document.getElementById('root')
+)
